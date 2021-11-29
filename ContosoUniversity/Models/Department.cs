2f426d0843a6dcad71047ae34102bcab69029e2e
Ben Bos <bennyb1097@yahoo.com>
2021-11-29 09:58:38 -0700

    test

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
-{
+{//
     public class Department
     {
         public int DepartmentID { get; set; }
